91a181839579be6d5ab4c4ae9368ddea5b1b4d86
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2023-06-28 18:02:25 +0200

    fetching current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let cityElement = document.querySelector("#currentCity");
+  cityElement.innerHTML = response.data.city;
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let winsSpeedElement = document.querySelector("#windSpeed");
+  winsSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "69fo350cf347a61tc6a94bf3497a464e";
+let city = "Pretoria";
+let unit = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
